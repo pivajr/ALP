dceb13695d65865dbef3a9f9b9b2a8919b6f4923
Dilermando Piva Jr <pivajr@gmail.com>
2024-04-01 22:14:14 -0300

    Aula 07 - atualização

---
+++ aula007/ex06.py
@@ -0,0 +1,14 @@
+termo = 0
+while (termo <= 0):
+    termo = int(input('Voce quer a série de Fibonacci até qual termo: '))
+    if (termo <= 0):
+        print('O termo deve ser positivo!')
+
+primeiro = 0
+print(primeiro)
+segundo = 1
+for i in range(1, termo):
+    print(segundo)
+    terceiro = primeiro + segundo
+    primeiro = segundo
+    segundo = terceiro
